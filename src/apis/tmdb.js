617cb143632ba2517f308c80f8b153ff8b9d33a1
altinthaqi <altin.thaqi.520@gmail.com>
2021-12-30 20:32:56 +0100

    Filter Movies, Page Pagination

--- src/apis/tmdb.js
+++ src/apis/tmdb.js
@@ -3,13 +3,15 @@ export const api = {
   base_url: "https://api.themoviedb.org/3",
   by_popularity: "/discover/movie?sort_by=popularity.desc",
   genres_list: "/genre/movie/list?",
-
-  by_genre: "/movie/horror",
   //Kur don me e bo search ni film
   search_movie: "/discover/movie?",
+  //Search by title https://api.themoviedb.org/3/search/movie?api_key=###&query=###
   search_title: "/search/movie?",
-  //query=avengers
   query: "&query=",
-  //Nihere qekjo, tani data.poster_path url-i image
+  //Change page, &page=n
+  page: "&page=",
+  //I shtohet: data.poster_path prej data obj
   original_img_url: "https://image.tmdb.org/t/p/original",
+  highest_rated: "sort_by=vote_count.desc",
+  lowest_rated: "sort_by=vote_average.asc",
 };
--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -3,29 +3,75 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { api } from "../apis/tmdb";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 function Movies() {
   const [data, setData] = useState([]);
   const [searchMovie, setSearchMovie] = useState("");
+  const [activeBtn, setActiveBtn] = useState("popular");
+  const [paginationPage, setPaginationPage] = useState(1);
+  const [watchLater, setWatchLater] = useState([]);
+
+  const makeApiCall = (params) => {
+    axios
+      .get(params)
+      .then((data) => setData(data.data.results))
+      .catch((err) => console.log(err));
+  };
 
   useEffect(() => {
     if (searchMovie === "") {
-      axios
-        .get(`${api.base_url}${api.by_popularity}&${api.key}`)
-        .then((data) => setData(data.data.results))
-        .catch((err) => console.log(err));
+      if (activeBtn === "popular") {
+        makeApiCall(
+          `${api.base_url}${api.by_popularity}&${api.key}${api.page}${paginationPage}`
+        );
+      } else if (activeBtn === "highest") {
+        makeApiCall(
+          `${api.base_url}${api.search_movie}${api.highest_rated}&${api.key}${api.page}${paginationPage}`
+        );
+      } else if (activeBtn === "lowest") {
+        makeApiCall(
+          `${api.base_url}${api.search_movie}${api.lowest_rated}&${api.key}${api.page}${paginationPage}`
+        );
+      }
     } else {
-      axios
-        .get(
+      const identifier = setTimeout(() => {
+        makeApiCall(
           `${api.base_url}${api.search_title}${api.key}${api.query}=${searchMovie}`
-        )
-        .then((data) => setData(data.data.results))
-        .catch((err) => console.log(err));
+        );
+      }, 1000);
+
+      return () => {
+        clearTimeout(identifier);
+      };
     }
-  }, [searchMovie]);
+  }, [searchMovie, paginationPage, activeBtn]);
 
   const inputHandler = (e) => {
     setSearchMovie(e.target.value);
+  };
+
+  const popularHandler = () => {
+    setActiveBtn("popular");
+    setPaginationPage(1);
+  };
+  const highestRatedHandler = () => {
+    setActiveBtn("highest");
+    setPaginationPage(1);
+  };
+  const lowestRatedHandler = () => {
+    setActiveBtn("lowest");
+    setPaginationPage(1);
+  };
+
+  const changePagination = (numb) => {
+    setPaginationPage(
+      paginationPage + numb >= 1 ? paginationPage + numb : paginationPage
+    );
+  };
+
+  const watchLaterHandler = () => {
+    setActiveBtn("watchLater");
   };
 
   return (
@@ -44,7 +90,32 @@ function Movies() {
             onChange={inputHandler}
           />
         </Intro>
-
+        <Options>
+          <button
+            className={activeBtn === "popular" ? "active" : ""}
+            onClick={popularHandler}
+          >
+            Most Popular
+          </button>
+          <button
+            className={activeBtn === "highest" ? "active" : ""}
+            onClick={highestRatedHandler}
+          >
+            Highest Rated
+          </button>
+          <button
+            className={activeBtn === "lowest" ? "active" : ""}
+            onClick={lowestRatedHandler}
+          >
+            Lowest Rated
+          </button>
+          <button
+            onClick={watchLaterHandler}
+            className={activeBtn === "watchLater" ? "active" : ""}
+          >
+            Watch Later
+          </button>
+        </Options>
         <MovieContainer>
           {data.map((m) => (
             <Movie key={m.id}>
@@ -53,10 +124,84 @@ function Movies() {
             </Movie>
           ))}
         </MovieContainer>
+
+        <Pagination>
+          <FaArrowLeft className="arrow" onClick={() => changePagination(-1)} />
+          {paginationPage > 1 && (
+            <p onClick={() => changePagination(-1)}>{paginationPage - 1}</p>
+          )}
+          <p className="first" onClick={() => changePagination(-1)}>
+            {paginationPage}
+          </p>
+
+          <p onClick={() => changePagination(+1)}>{paginationPage + 1}</p>
+          <FaArrowRight
+            className="arrow"
+            onClick={() => changePagination(+1)}
+          />
+        </Pagination>
       </Container>
     </>
   );
 }
+
+const Pagination = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 100px;
+  background-color: ${(props) => props.theme.body};
+
+  p {
+    margin: 0px 20px;
+    color: ${(props) => props.theme.text};
+    cursor: pointer;
+  }
+
+  .first {
+    font-size: 18px;
+    font-weight: 900;
+  }
+
+  .arrow {
+    cursor: pointer;
+    color: ${(props) => props.theme.text};
+  }
+`;
+
+const Options = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  background-color: ${(props) => props.theme.body};
+  /* color: ${(props) => props.theme.text}; */
+
+  button {
+    padding: 8px;
+    margin: 10px;
+    background-color: ${(props) => props.theme.body};
+    color: ${(props) => props.theme.text};
+
+    font-weight: 600;
+    border: none;
+    cursor: pointer;
+    transition: all 0.5s;
+    border: 1px solid ${(props) => props.theme.text};
+
+    &:hover {
+      transition: all 0.5s;
+      background-color: ${(props) => props.theme.text};
+      color: ${(props) => props.theme.body};
+      border: 1px solid ${(props) => props.theme.text};
+    }
+  }
+
+  .active {
+    background-color: ${(props) => props.theme.text};
+    color: ${(props) => props.theme.body};
+  }
+`;
 
 const Intro = styled.div`
   width: 100%;
@@ -97,6 +242,7 @@ const Intro = styled.div`
     padding: 10px;
     width: 50%;
     max-width: 300px;
+    border: 1px solid black;
   }
 `;
 
